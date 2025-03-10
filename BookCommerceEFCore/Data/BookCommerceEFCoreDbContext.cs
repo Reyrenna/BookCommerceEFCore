cca2df9dba4d5adef0324d30816f1cf7cc4a5181
Reyett___ <reyrenna4@gmail.com>
2025-03-10 19:17:27 +0100

    Aggiungere i file di progetto.

---
+++ BookCommerceEFCore/Data/BookCommerceEFCoreDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookCommerceEFCore.Models;
+using Microsoft.EntityFrameworkCore;
+namespace BookCommerceEFCore.Data
+{
+    public class BookCommerceEFCoreDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public BookCommerceEFCoreDbContext(DbContextOptions<BookCommerceEFCoreDbContext> options) : base(options) { 
+        
+        }
+        
+    }
+}
---
+++ BookCommerceEFCore/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookCommerceEFCore.Models
+{
+    public class Book
+    {
+        [Key]
+        public required Guid Id { get; set; }
+
+        [Required]
+        [StringLength(300)]
+        public required string Title { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public required string Author { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public required string Genre { get; set; }
+
+        [Required]
+        public required bool IsStock { get; set; }
+
+        public string? ImageUrl { get; set; }
+    }
+}
